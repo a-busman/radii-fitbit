fdb38408a5752ed5a6bf07ae48a04c711cc00fab
Alex Busman <alex.busman@gmail.com>
2019-02-14 12:54:04 -0800

    Add steps goal to display

--- app/steps.js
+++ app/steps.js
@@ -4,7 +4,7 @@ import { goals } from "user-activity"
 import { today } from "user-activity"
 
 let stepLabel = document.getElementById("step-count");
-
+let stepRemainingLabel = document.getElementById("steps-remaining");
 function zeroPad(i) {
   if (i < 10) {
     return "00" + i;
@@ -16,11 +16,20 @@ function zeroPad(i) {
 
 export function update() {
   let steps = today.local.steps;
+  let remainingSteps = goals.steps - today.local.steps;
   
   if (steps >= 1000) {
     stepLabel.text = `${Math.floor(steps / 1000)},${zeroPad(steps % 1000)}`;
   } else {
     stepLabel.text = `${steps}`;
+  }
+  
+  if (remainingSteps >= 1000) {
+    stepRemainingLabel.text = `${Math.floor(remainingSteps / 1000)},${zeroPad(remainingSteps % 1000)}`;
+  } else if (remainingSteps <= 0) {
+    stepRemainingLabel.text = "0";
+  } else {
+    stepRemainingLabel.text = `${remainingSteps}`;
   }
 }
 
